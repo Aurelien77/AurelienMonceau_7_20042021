0ae7b6b547a31a7a961f02a5738ef9130f713976
Aurélien <aurelien.monceau@gmail.com>
2021-10-14 15:03:10 +0200

    Initailisation du framwork react + récupération des infos de la BD via la route http://localhost:3001/posts

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const app = express();
-/* const cors = require("cors"); */
+const postRouter = require("./routes/Posts");
+const cors = require("cors");
 
-/* app.use(express.json());
+app.use(express.json());
 app.use(cors());
- */
+
 const db = require("./models"); // Pour injecter le modèle dans la BD
 
-// Routers
-/* const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-const commentsRouter = require("./routes/Comments");
-app.use("/comments", commentsRouter);
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
-const likesRouter = require("./routes/Likes");
-app.use("/likes", likesRouter); */
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
