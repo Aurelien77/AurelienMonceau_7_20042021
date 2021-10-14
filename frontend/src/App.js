123fdd9c04ea84abda1f8bb528975cd2f7e68e6b
Aurélien <aurelien.monceau@gmail.com>
2021-10-14 15:32:53 +0200

    Initialisation du framwork react + récupération des infos de la BD via la route http://localhost:3001/posts

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+function App() {
+  const [listOfPosts, SetListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      SetListOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {" "}
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
