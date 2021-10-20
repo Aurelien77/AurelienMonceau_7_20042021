a214d65135f274758eb0fa3a2df85c5543c65510
Aurélien <aurelien.monceau@gmail.com>
2021-10-20 14:34:35 +0200

    Déplacement de la logique métier dans des controllers

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,75 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models");
-const bcrypt = require("bcrypt");
 const { validateToken } = require("../middlewares/AuthMiddleware");
-const { sign } = require("jsonwebtoken");
+const userCtrl = require("../controllers/users");
 
-router.post("/", async (req, res) => {
-  const { username, password, email } = req.body;
-  bcrypt.hash(password, 10).then((hash) => {
-    Users.create({
-      username: username,
-      password: hash,
-      email: email,
-      admin: false,
-    });
-    res.json("SUCCESS");
-  });
-});
+router.post("/login", userCtrl.login);
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+router.get("/auth", validateToken, userCtrl.auth);
 
-  const user = await Users.findOne({ where: { username: username } });
+router.post("/", userCtrl.signup);
 
-  if (!user) res.json({ error: "User Doesn't Exist" });
+router.get("/basicinfo/:id", userCtrl.basicInfo);
 
-  bcrypt.compare(password, user.password).then(async (match) => {
-    if (!match) res.json({ error: "Wrong Username And Password Combination" });
-
-    const accessToken = sign(
-      { username: user.username, id: user.id },
-      "importantsecret"
-    );
-    res.json({
-      token: accessToken,
-      username: username,
-      id: user.id,
-      admin: user.admin,
-    });
-  });
-});
-
-router.get("/auth", validateToken, (req, res) => {
-  res.json(req.user);
-});
-
-router.get("/basicinfo/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const basicInfo = await Users.findByPk(id, {
-    attributes: { exclude: ["password"] },
-  });
-
-  res.json(basicInfo);
-});
-
-router.put("/changepassword", validateToken, async (req, res) => {
-  const { oldPassword, newPassword } = req.body;
-  const user = await Users.findOne({ where: { username: req.user.username } });
-
-  bcrypt.compare(oldPassword, user.password).then(async (match) => {
-    if (!match) res.json({ error: "Wrong Password Entered!" });
-
-    bcrypt.hash(newPassword, 10).then((hash) => {
-      Users.update(
-        { password: hash },
-        { where: { username: req.user.username } }
-      );
-      res.json("SUCCESS");
-    });
-  });
-});
+router.put("/changepassword", validateToken, userCtrl.changepassword);
 
 module.exports = router;
